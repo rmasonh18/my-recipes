ce8aebb26e05f718dc12db5a9a25a6d940779597
rmasonh18 <rh12@emil.sc.edu>
2019-09-05 01:48:36 -0400

    string interpolation & property binding

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -5,5 +5,11 @@ import { Component } from '@angular/core';
     templateUrl: './server.component.html'
 })
 export class ServerComponent {
+    serverId = 10; // serverId: number =
+    serverStatus = "offline"; //serverStatus: string =
+    // add method
+    getServerStatus() {
+        return this.serverStatus;
+    }
 
 }
