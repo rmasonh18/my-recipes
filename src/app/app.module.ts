f5b90d6ea4f86aa84b5716b685c0c5a0252ab1db
rmasonh18 <rh12@emil.sc.edu>
2019-09-05 01:14:14 -0400

    add some styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
+import { SuccessalertComponent } from './successalert/successalert.component';
+import { WarningalertComponent } from './warningalert/warningalert.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    ServersComponent
+    ServersComponent,
+    SuccessalertComponent,
+    WarningalertComponent
   ],
   imports: [
     BrowserModule
--- src/app/bootstrap.md
+++
@@ -1,5 +0,0 @@
-# Bootstrap add to project
-1. ng new \<projectname>
-2. cd \<projectname>
-3. ng serve
-4. open new teminal window
